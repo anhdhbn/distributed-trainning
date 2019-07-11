a8bcbca78426d9865f0dd46d93e581443ae4fd1e
AnhDH <anhdhbn@gmail.com>
2019-07-12 03:24:36 +0700

    test

--- model.py
+++ model.py
@@ -8,14 +8,22 @@ from keras.layers import Dense, Conv2D, Flatten, MaxPooling2D, Dropout
 
 
 def make_model():
-    model = Sequential()
-    model.add(Conv2D(32, kernel_size=3, activation='relu', input_shape=(28,28,1)))
-    model.add(Conv2D(64, kernel_size=3, activation='relu'))
-    model.add(MaxPooling2D((2, 2)))
-    model.add(Flatten())
-    model.add(Dense(128, activation='relu'))
-    model.add(Dropout(0.25))
-    model.add(Dense(10, activation='softmax'))
+    LABEL_DIMENSIONS = 10
+    inputs = tf.keras.Input(shape=(28,28,1))  
+    x = tf.keras.layers.Conv2D(filters=32, kernel_size=(3, 3), activation='relu')(inputs)
+    x = tf.keras.layers.MaxPooling2D(pool_size=(2, 2), strides=2)(x)
+    x = tf.keras.layers.Conv2D(filters=64, kernel_size=(3, 3), activation='relu')(x)
+    x = tf.keras.layers.MaxPooling2D(pool_size=(2, 2), strides=2)(x)
+    x = tf.keras.layers.Conv2D(filters=256, kernel_size=(3, 3), activation='relu')(x)
+    x = tf.keras.layers.Flatten()(x)
+    x = tf.keras.layers.Dense(64, activation='relu')(x)
+    x = tf.keras.layers.Dropout(0.2)(x)
+    x = tf.keras.layers.Dense(128, activation='relu')(x)
+
+    predictions = tf.keras.layers.Dense(LABEL_DIMENSIONS, activation='softmax')(x)
+
+    model = tf.keras.Model(inputs=inputs, outputs=predictions)
+    model.summary()
     return model
 
 
